12d20b023d36317e3f8d431dd56651e656a12736
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2017-10-17 14:19:55 +0700

    Handling insider function

--- public/react.js
+++ public/react.js
@@ -27,6 +27,55 @@ function Sum(props) {
     );
 }
 
+function HandlingEvent() {
+    function ActiveLink(e) {
+        e.preventDefault();
+        console.log('The link was click')
+    }
+    return(
+        <div>
+            <a href="#" onClick={ActiveLink}>Click on me</a>
+        </div>
+    );
+}
+
+class ButtonAlert extends React.Component {
+    getInfo(){
+        alert(this.props.children);
+    }
+    render(){
+        return(
+            <div>
+                <button onClick={this.getInfo}>Click me </button> <br/>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+class Toggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isToggleOn: true};
+
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
+
+    render() {
+        return (
+            <button >
+                {this.state.isToggleOn ? 'ON' : 'OFF'}
+            </button>
+        );
+    }
+}
 
 ReactDOM.render(
     <h1 className="renderFirst"> Web Developer </h1>,
@@ -34,8 +83,15 @@ ReactDOM.render(
 
 ReactDOM.render(
     <div>
-        <TestRender/>
+        <TestRender />
         <Sum result="10" solution="xyz">Children Props Test </Sum>
+        Handling Event
+        <HandlingEvent />
+        Button Alert
+        <ButtonAlert> You're clicked on Button </ButtonAlert>
+        Toggle <br/>
+        <Toggle />
+
     </div>,
     document.getElementById('content')
 );
