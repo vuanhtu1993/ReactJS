7840f6ca7b13cb8c59ccecc2b955427194d529f9
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2017-10-22 18:32:59 +0700

    Declare State, Adjust State, Render State

--- public/EventOutFunction.js
+++ public/EventOutFunction.js
@@ -3,18 +3,46 @@ function EventOut(name) {
 }
 
 class ButtonAlert extends React.Component {
-    getInfo(param){
-       alert(param)
-    }
-    render(){
-        return(
+
+    render() {
+        return (
             <div>
-                <button onClick={() => EventOut(this.props.children)}>Click me</button><br/>
+                <button onClick={() => EventOut(this.props.children)}>Click me</button>
+                <br/>
                 {this.props.children}
             </div>
         );
     }
 }
+
+class Scuti extends React.Component {
+    // STATE
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date(),
+            member: 10,
+        };
+    }
+    // adjust STATE
+    changeState() {
+        this.setState({
+           member: this.state.member + 1,
+        });
+
+    };
+    // arrow function again
+    render() {
+        return (
+            <div>
+                {this.state.member}
+                <button onClick={() => this.changeState()}> Add member</button>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+}
+
 // arrow function là cách định nghĩa nhanh một function, Tức là thực thi arrow function trước khi thực thi các hàm trong arrow function
 ReactDOM.render(
     <h1 className="renderFirst"> Web Developer </h1>,
@@ -24,7 +52,7 @@ ReactDOM.render(
 ReactDOM.render(
     <div>
         <ButtonAlert>EventOut Function </ButtonAlert>
+        <Scuti> </Scuti>
     </div>,
-
     document.getElementById('content'),
 );
